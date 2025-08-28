2c46f05975e6bfe8b4e3d8497a24c908d71d9974
Duy Kha Nguyen <nguyenduykha12762@gmail.com>
2025-08-28 11:21:37 +0700

    add Bai12.22

--- Bai12/payment-stripe/readme.md
+++ Bai12/payment-stripe/readme.md
@@ -14,3 +14,24 @@ npm install express stripe dotenv
 STRIPE_PUBLISHABLE_KEY=pk_test_51Ny7xyK3Pu
 STRIPE_SECRET_KEY=sk_test_51Ny7xyK3P
 ```
+
+# Docs
+https://docs.stripe.com/payments/quickstart
+https://dashboard.stripe.com/test/dashboard
+https://docs.stripe.com/testing
+
+# Install Stripe CLI
+https://docs.stripe.com/stripe-cli
+
+Terminal 1:
+If not login then **Press Enter to open the browser**
+
+```sh
+stripe listen --forward-to localhost:3000/stripe-webhook
+```
+After run this command, Stripe return **Webhook Signing Secret**. You copy this key to .env file.
+
+Terminal 2:
+```sh
+stripe trigger payment_intent.succeeded
+```
--- Bai12/payment-stripe/server.js
+++ Bai12/payment-stripe/server.js
@@ -4,6 +4,34 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const app = express();
 
+app.post("/stripe-webhook", express.raw({ type: "application/json" }), async (req, res) => {
+    const sig = req.headers['stripe-signature'];
+
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (error) {
+        console.log(`Webhook signature verification failed: ${error.message}`);
+        return res.sendStatus(400);
+    }
+
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntentSucceeded = event.data.object;
+            console.log("paymentIntentSucceeded:", paymentIntentSucceeded);
+            break;
+        case 'payment_intent.payment_failed':
+            const paymentIntentFailed = event.data.object;
+            console.log("paymentIntentFailed:", paymentIntentFailed);
+            break;
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+    res.json({ received: true });
+})
+
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -15,7 +43,7 @@ app.post('/create-payment-intent', async (req, res) => {
     }
 
     try {
-        const paymentIntent = await stripe.paymentIntent.create({
+        const paymentIntent = await stripe.paymentIntents.create({
             amount: calculateOrderAmount(items),
             currency: currency || 'usd',
             automatic_payment_methods: {
